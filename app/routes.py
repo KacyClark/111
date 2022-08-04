a282137885fca3295f429309697884fac6f3179b
Kacy Clark <kacy@usa.com>
2022-08-03 23:34:59 -0400

    Genesis

---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from flask import Flask  # we imported Flask class 
+
+app = Flask(__name__)# app is an instance
+
+@app.route("/")   #this is a route
+def index():    # our view function
+    return "<h1>Hello world</h1>"  # return string
+
+@app.route("/about")
+def about(): 
+    me = {
+        "first_name": "Kacy",
+        "last_name": "Clark",
+        "hobby": "raise dogs",
+        "former_job": "critical_care_RN"
+    }
+    return me
+
+@app.route("/dream_mate")
+def mate():
+    he = {
+        "height": "tall",
+        "build": "muscular",
+        "skin": "dark",
+        "educated": "hard_knocks"
+    }    
+    return he        
+    
